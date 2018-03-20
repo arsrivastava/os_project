e91a9e39bd3d803fc20ddf00f46580c1f7d95e5e
arsrivastava <37588253+arsrivastava@users.noreply.github.com>
2018-03-20 22:28:38 +0530

    Create round_robin.c

---
+++ round_robin.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<fcntl.h>
+int main()
+{
+int number,process[10],queue[50],flag=0,time_quantum,arrival[],brust_time[50],i,j,k,l;
+printf("enter no of processes");
+scanf("%d",&number);
+for(int i=1;i<=n;i++)
+{
+printf("Arrival time and cpu time brust for process",i);
+scanf("%d",&arrival[i]);
+scanf("%d",&brust_time);
+}
